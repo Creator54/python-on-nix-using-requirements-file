dfed8e1b8a037cde81167f503343b7ebea55d414
creator54 <hi.creator54@gmail.com>
2022-12-04 18:34:42 +0530

    updates added nix-shell functions, ignore .venv/, update python -> 3.10

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ pkgs.mkShell rec {
   name = "impurePythonEnv";
   venvDir = "./.venv";
   buildInputs = [
-    python38Packages.python #set your python interpreter for the project
+    python310
   ];
 
   shellHook = ''
@@ -17,5 +17,20 @@ pkgs.mkShell rec {
     source "${venvDir}/bin/activate"
     python -m pip install --upgrade pip
     pip install -r requirements.txt
+    eval "$extras"
+  '';
+  extras = ''
+    alias pymod="pip list" #show installed modules
+
+    pyadd(){ # add package to requirements.txt so it will be commited
+      if ! cat requirements.txt | grep $1 &>/dev/null;then
+        echo $1 >> requirements.txt
+        pip3 install -r requirements.txt
+      fi
+    }
+    pyrm(){ #remove package from requirements.txt so it won't be commited
+      cat requirements.txt | sed -i "/$1/d" requirements.txt
+      pip uninstall $1 -y
+    }
   '';
 }
