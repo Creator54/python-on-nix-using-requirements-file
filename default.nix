0b77c257f35d525e1b1c3d083a450fa6b7c43611
creator54 <hi.creator54@gmail.com>
2023-08-28 17:16:13 +0530

    cleanup -       fixed extra functions on nix-shell -       nix-shell now autocreates virtual env -       do not use caches

--- default.nix
+++ default.nix
@@ -1,7 +1,6 @@
 with import <nixpkgs> { };
 pkgs.mkShell rec {
   name = "impurePythonEnv";
-  venvDir = "./.venv";
   buildInputs = [
     python310
   ];
@@ -9,15 +8,20 @@ pkgs.mkShell rec {
   shellHook = ''
     set -h #remove "bash: hash: hashing disabled" warning !
     SOURCE_DATE_EPOCH=$(date +%s)
-
-    if ! [ -d "${venvDir}" ]; then
-      python -m venv "${venvDir}"
-    fi
     export LD_LIBRARY_PATH="${lib.makeLibraryPath [ zlib stdenv.cc.cc ]}":LD_LIBRARY_PATH;
-    source "${venvDir}/bin/activate"
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt
     eval "$extras"
+
+    if ! [ -d .venv ]; then
+      python -m venv .venv
+    fi
+
+    source .venv/bin/activate
+
+    python -m pip install --upgrade pip
+
+    if [ -e requirements.txt ]; then
+      pip install -r requirements.txt
+    fi
   '';
   extras = ''
     pymod() {
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#https://dev.to/allenap/some-direnv-best-practices-actually-just-one-4864
-if ! [[ -f ./.envrc.cache ]]; then
-  nix-shell --run 'direnv dump > .envrc.cache'
-fi
